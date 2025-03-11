588a5006bcbebf6cf1a4a961b25837bded09f6bf
mert-kurttutan <kurttutan.mert@gmail.com>
2025-03-11 01:33:02 +0100

    Use actual python version info from json

--- scripts/get_version.py
+++ scripts/get_version.py
@@ -16,10 +16,19 @@ if __name__ == '__main__':
     # take single cli integer argument
     parser = argparse.ArgumentParser(description="This script takes a single integer argument")
     parser.add_argument("integer", type=int, help="an integer")
+    # add option to print numpy or python version
+    parser.add_argument("--variable-name", type=str, help="numpy or python")
     args = parser.parse_args()
     version_id = args.integer
 
     file_path = '.github/version_map.json'
     version_dict = get_version_dict(file_path)
     version_list = flatten_dict(version_dict)
-    print(f"version with id: {version_id} is {version_list[version_id]}")
+    # print(f"version with id: {version_id} is {version_list[version_id]}")
+    variable_name = args.variable_name
+    if variable_name == "numpy":
+        print(f"{version_list[version_id]['numpy_version']}")
+    elif variable_name == "python":
+        print(f"{version_list[version_id]['python_version']}")
+    else:
+        raise ValueError("variable-name should be numpy_version or python_version")
