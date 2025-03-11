50d95ffaa8c38331f8ad151cb7a2168405d63314
mert-kurttutan <kurttutan.mert@gmail.com>
2025-03-11 01:00:25 +0100

    New version scheme

---
+++ scripts/get_version.py
@@ -0,0 +1,10 @@
+
+
+if __name__ == '__main__':
+    # take single integer input
+    n = int(input())
+    # take list of integers as input
+    if n == 0:
+        print(f"id: {n}")
+    else:
+        print(f"id not zero: {n}")
